0cc6848be91da19cd85de5d7f6be8427fdb1eef7
Daniel Lerch <daniel@daniellerch.ch>
2021-01-14 11:15:33 +0100

    deployes sanity studio

--- web/.next/static/chunks/webpack.js
+++ web/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "66c7e1ae81a65c6c8d89";
+/******/ 	var hotCurrentHash = "216f57928ba2688b890b";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
