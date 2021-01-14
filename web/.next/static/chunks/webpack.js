e29372092528dd962337465158ba3dd2f5e3fd82
Daniel Lerch <daniel@daniellerch.ch>
2021-01-14 10:51:05 +0100

    refines mobile only view

--- web/.next/static/chunks/webpack.js
+++ web/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "ad69932bf3a2ff199c0a";
+/******/ 	var hotCurrentHash = "d6990f94964a9a752f6f";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
