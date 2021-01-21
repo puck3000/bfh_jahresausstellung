2a6701fed1d70271561d0b5982daa626d7840a8e
Daniel Lerch <daniel@daniellerch.ch>
2021-01-21 10:01:53 +0100

    fixes team portrait alt-title

--- web/.next/static/chunks/webpack.js
+++ web/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "565342d3731a153d3612";
+/******/ 	var hotCurrentHash = "9414c11be0e1ba55669e";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- web/components/Team.js
+++ web/components/Team.js
@@ -1,31 +1,28 @@
 import { urlFor } from 'lib/sanity'
 
-
-export default function Team({team}) {
-    const teammitglieder = team.map((teammitglied) => (
-        <li>
-            <img
-            src={urlFor(teammitglied.avatar)
-                .width(2000)
-                .height(1600)
-                .format('webp')
-                .url()}
-                srcSet={
-                    `${urlFor(teammitglied.avatar)
-                        .width(1024)
-                        .height(819)
-                        .format('webp')
-                        .url()} 1024w, ${urlFor(teammitglied.avatar)
-                            .width(2000)
-                            .height(1600)
-                            .format('webp')
-                            .url()} 2000w,`
-                        }
-                        sizes="(max-width:1024px) 100vw, 50vw"
-                        alt="teammitglied.person"
-            />  
-            <p>{teammitglied.person}</p>
-        </li> 
-    ))
-    return <ul className="grid grid-cols-2 gap-1 mb-2">{teammitglieder}</ul>
+export default function Team({ team }) {
+  const teammitglieder = team.map((teammitglied) => (
+    <li>
+      <img
+        src={urlFor(teammitglied.avatar)
+          .width(2000)
+          .height(1600)
+          .format('webp')
+          .url()}
+        srcSet={`${urlFor(teammitglied.avatar)
+          .width(1024)
+          .height(819)
+          .format('webp')
+          .url()} 1024w, ${urlFor(teammitglied.avatar)
+          .width(2000)
+          .height(1600)
+          .format('webp')
+          .url()} 2000w,`}
+        sizes='(max-width:1024px) 100vw, 50vw'
+        alt={`Portrait von ${teammitglied.person}`}
+      />
+      <p>{teammitglied.person}</p>
+    </li>
+  ))
+  return <ul className='grid grid-cols-2 gap-1 mb-2'>{teammitglieder}</ul>
 }
