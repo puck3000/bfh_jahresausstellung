72a7888ef8a8576ac387ab880f0bf30ce0c6b174
Daniel Lerch <daniel@daniellerch.ch>
2021-02-01 11:48:14 +0100

    removew standard wepb

--- web/components/Linkedpic.js
+++ web/components/Linkedpic.js
@@ -1,39 +1,30 @@
 import client from 'client'
-import BlockContent from '@sanity/block-content-to-react' 
+import BlockContent from '@sanity/block-content-to-react'
 import imageUrlBuilder from '@sanity/image-url'
 
-function urlFor (source) {
-    return imageUrlBuilder(client).image(source)
-  }
+function urlFor(source) {
+  return imageUrlBuilder(client).image(source)
+}
 
-export default function Linkedpic({linkedpic}) {
-    return (
-        <figure className="mb-2">
-                <img
-                  src={urlFor(linkedpic.image)
-                    .width(2000)
-                    .height(1600)
-                    .format('webp')
-                    .url()}
-                  srcSet={
-                    `${urlFor(linkedpic.image)
-                      .width(1024)
-                      .height(819)
-                      .format('webp')
-                      .url()} 1024w, ${urlFor(linkedpic.image)
-                        .width(2000)
-                        .height(1600)
-                        .format('webp')
-                        .url()} 2000w,`
-                    }
-                  sizes="(max-width:1024px) 100vw, 50vw"
-                />    
-                <figcaption>
-                  <BlockContent 
-                    blocks={linkedpic.caption}
-                    {...client.config()}
-                  />
-                </figcaption>  
-              </figure>
-    )
+export default function Linkedpic({ linkedpic }) {
+  return (
+    <figure className='mb-2'>
+      <img
+        src={urlFor(linkedpic.image).width(2000).height(1600).url()}
+        srcSet={`${urlFor(linkedpic.image)
+          .width(1024)
+          .height(819)
+          .format('webp')
+          .url()} 1024w, ${urlFor(linkedpic.image)
+          .width(2000)
+          .height(1600)
+          .format('webp')
+          .url()} 2000w,`}
+        sizes='(max-width:1024px) 100vw, 50vw'
+      />
+      <figcaption>
+        <BlockContent blocks={linkedpic.caption} {...client.config()} />
+      </figcaption>
+    </figure>
+  )
 }
