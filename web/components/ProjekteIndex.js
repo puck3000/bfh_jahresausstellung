1ee811b9b5d2cd2a982462af18cbd105c7197e8d
Daniel Lerch <daniel@daniellerch.ch>
2021-02-08 10:49:17 +0100

    adds col4 view on big projIndex

--- web/components/ProjekteIndex.js
+++ web/components/ProjekteIndex.js
@@ -3,7 +3,7 @@ import ProjLightBox from './ProjLightBox'
 
 export default function ProjekteIndex({ projekte }) {
   return (
-    <ul className='grid grid-cols-2 gap-1'>
+    <ul className='grid grid-cols-2 lg:grid-cols-4 gap-1'>
       {projekte.map((projekt) => (
         <li key={projekt._id}>
           {projekt.content.gallery ? (
