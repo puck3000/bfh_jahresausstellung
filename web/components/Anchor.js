1933087bc809c63156a8ee30fccc154d1c5b1e5c
Daniel Lerch <daniel@daniellerch.ch>
2021-02-11 15:04:14 +0100

    solved the map puzzle

--- web/components/Anchor.js
+++ web/components/Anchor.js
@@ -1,6 +1,9 @@
 export default function Anchor({ anchor }) {
   return (
-    <div key={anchor._key} className='anker mb-1 lg:mb-2'>
+    <div
+      key={anchor._key}
+      className='anker mb-1 sticky top-10 lg:top-16 2xl:top-28 lg:mb-2'
+    >
       {anchor.anchorTag}
     </div>
   )
