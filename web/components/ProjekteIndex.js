744fdbcfd1e82dc234c35e5e49552575228b6780
Daniel Lerch <daniel@daniellerch.ch>
2021-02-11 16:18:29 +0100

    done akkordeons

--- web/components/ProjekteIndex.js
+++ web/components/ProjekteIndex.js
@@ -9,12 +9,6 @@ export default function ProjekteIndex({ projekte }) {
         <div>
           <h2 className='anker first mb-1 lg:mb-2 lg:inline-block'>Projekte</h2>
         </div>
-        <button className='h-8 focus:outline-none lg:col-start-2 lg:col-span-3 lg:justify-self-start '>
-          <p className='hidden lg:inline mr-3 '>
-            Projekte in der Kartenansicht öffnen:
-          </p>
-          <MdMap size='2rem' className='inline' />
-        </button>
       </div>
       <ul className='grid grid-cols-2 lg:grid-cols-4 gap-1'>
         {projekte.map((projekt) => (
