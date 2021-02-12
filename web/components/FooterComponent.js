6cf8c071eaab67607c3b95fd47edf72041be7672
Daniel Lerch <daniel@daniellerch.ch>
2021-02-12 02:48:24 +0100

    footer done

--- web/components/FooterComponent.js
+++ web/components/FooterComponent.js
@@ -3,80 +3,82 @@ import Image from 'next/image'
 
 export default function FooterComponent() {
   return (
-    <footer className='mb-two lg:mb-big'>
+    <footer className=''>
       <hr className='mt-two lg:mt-big mb-2' />
       <ul className='lg:grid lg:gap-1 lg:grid-cols-8'>
         <li className='mb-2 lg:col-span-2'>
-          <p className='mb-1 lg:mb-2'>Ein Projekt der</p>
-          {/* <Link href='/'>
-            <Image
+          <p className='mb-1  lg:mb-2 text-small lg:text-small-dt'>
+            Ein Projekt der
+          </p>
+          <Link href='/'>
+            <img
               src='/logos/logo_hkb_footer_100px.svg'
               alt='Logo BFH'
-              width={100}
-              height={100}
+              className='h-24'
             />
-          </Link> */}
+          </Link>
         </li>
-        <li className='mb-2 lg:col-span-4'>
-          <p className='mb-1 lg:mb-2'>Sponsoren</p>
-          {/* <ul className='grid grid-cols-2 lg:grid-cols-5'>
-            <li className='cursor-pointer'>
+        <li className='mb-2 lg:col-span-4 lg:flex lg:flex-col lg:justify-between'>
+          <p className='mb-4 mt-one lg:mt-0 text-small lg:text-small-dt  '>
+            Sponsoren
+          </p>
+          <ul className='lg:flex lg:flex-row '>
+            <li className='cursor-pointer mb-4 lg:mb-0 lg:mr-4'>
               <Link href='/'>
-                <Image
+                <img
                   src='/logos/1_HERZOG_footer_65px.svg'
                   alt='Logo Herzog'
-                  width={65}
-                  height={65}
+                  className='h-16'
                 />
               </Link>
             </li>
-            <li className='cursor-pointer'>
+            <li className='cursor-pointer  mb-4 lg:mb-0 lg:mr-4 '>
               <Link href='/'>
-                <Image
+                <img
                   src='/logos/1_LoMarazzi_footer_65px.svg'
                   alt='Logo Herzog'
-                  width={76}
-                  height={65}
+                  className='h-16'
                 />
               </Link>
             </li>
-            <li className='cursor-pointer'>
+            <li className='cursor-pointer  mb-4 lg:mb-0 lg:mr-4'>
               <Link href='/'>
-                <Image
+                <img
                   src='/logos/1_Hutwil_footer_65px.png'
                   alt='Logo Huttwil'
-                  width={65}
-                  height={65}
+                  className='h-16'
                 />
               </Link>
             </li>
-            <li className='cursor-pointer'>
+            <li className='cursor-pointer  mb-4 lg:mb-0 lg:mr-4'>
               <Link href='/'>
-                <Image
+                <img
                   src='/logos/1_SIA_footer_65px.png'
                   alt='Logo SIA'
-                  width={65}
-                  height={65}
+                  className='h-16'
                 />
               </Link>
             </li>
-            <li className='cursor-pointer'>
+            <li className='cursor-pointer  mb-4 lg:mb-0 lg:mr-4'>
               <Link href='/'>
-                <Image
+                <img
                   src='/logos/1_Kornhausforum_footer_65px.svg'
                   alt='Logo Kornhausforum'
-                  width={65}
-                  height={65}
+                  className='h-16'
                 />
               </Link>
             </li>
-          </ul> */}
+          </ul>
         </li>
         <li className='mb-2 lg:col-span-1'>
-          <Link href='/impressum'>Impressum</Link>
+          <Link href='/impressum'>
+            <a className='text-small lg:text-small-dt'>Impressum</a>
+          </Link>
         </li>
         <li className='mb-2 lg:col-span-1'>
-          <Link href='/kontakt'>Kontakt</Link>
+          <Link href='/kontakt'>
+            <a className='text-small lg:text-small-dt'>Kontakt</a>
+          </Link>
         </li>
       </ul>
     </footer>
