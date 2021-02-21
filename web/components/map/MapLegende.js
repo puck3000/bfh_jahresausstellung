02259d105b50a8f340c0522bb83fc448fb2fd2a7
Daniel Lerch <daniel@daniellerch.ch>
2021-02-21 02:32:18 +0100

    unselect standort with click on legend

--- web/components/map/MapLegende.js
+++ web/components/map/MapLegende.js
@@ -31,6 +31,7 @@ export default function MapLegende() {
     setMapContext((mapContext) => ({
       ...mapContext,
       filteredStandorte: filteredStandorte,
+      selectedStandort: '',
     }))
   }, [filter])
 
