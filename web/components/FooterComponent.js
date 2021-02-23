94e5e95cb6e664b697ec5dd7e8996fd62f8e9d72
Daniel Lerch <daniel@daniellerch.ch>
2021-02-23 11:59:51 +0100

    footer finish

--- web/components/FooterComponent.js
+++ web/components/FooterComponent.js
@@ -7,9 +7,9 @@ export default function FooterComponent() {
       <hr className='mt-two lg:mt-big mb-2' />
       <ul className='lg:grid lg:gap-1 lg:grid-cols-8'>
         <li className='mb-2 lg:col-span-2'>
-          <p className='mb-1  lg:mb-2 text-small lg:text-small-dt'>
+          {/* <p className='mb-1  lg:mb-2 text-small lg:text-small-dt'>
             Ein Projekt der
-          </p>
+          </p> */}
           <Link href='/'>
             <img
               src='/logos/logo_hkb_footer_100px.svg'
@@ -24,49 +24,52 @@ export default function FooterComponent() {
           </p>
           <ul className='lg:flex lg:flex-row '>
             <li className='cursor-pointer mb-4 lg:mb-0 lg:mr-4'>
-              <Link href='/'>
+              <a href=' https://www.herzogbau.ch/' target='_blank'>
                 <img
                   src='/logos/1_HERZOG_footer_65px.svg'
                   alt='Logo Herzog'
-                  className='h-16'
+                  className='h-14 mr-4'
                 />
-              </Link>
+              </a>
             </li>
             <li className='cursor-pointer  mb-4 lg:mb-0 lg:mr-4 '>
-              <Link href='/'>
+              <a href='https://www.losinger-marazzi.ch/de/' target='_blank'>
                 <img
                   src='/logos/1_LoMarazzi_footer_65px.svg'
-                  alt='Logo Herzog'
-                  className='h-16'
+                  alt='Logo Losinger Marazzi'
+                  className='h-14 mr-4'
                 />
-              </Link>
+              </a>
             </li>
             <li className='cursor-pointer  mb-4 lg:mb-0 lg:mr-4'>
-              <Link href='/'>
+              <a href='http://www.huttwil.ch/' target='_blank'>
                 <img
                   src='/logos/1_Hutwil_footer_65px.png'
                   alt='Logo Huttwil'
-                  className='h-16'
+                  className='h-14 mr-4'
                 />
-              </Link>
+              </a>
             </li>
             <li className='cursor-pointer  mb-4 lg:mb-0 lg:mr-4'>
-              <Link href='/'>
+              <a
+                href='https://www.sia.ch/de/der-sia/sektionen/'
+                target='_blank'
+              >
                 <img
                   src='/logos/1_SIA_footer_65px.png'
                   alt='Logo SIA'
-                  className='h-16'
+                  className='h-14 mr-4'
                 />
-              </Link>
+              </a>
             </li>
             <li className='cursor-pointer  mb-4 lg:mb-0 lg:mr-4'>
-              <Link href='/'>
+              <a href='https://www.kornhausforum.ch/' target='_blank'>
                 <img
                   src='/logos/1_Kornhausforum_footer_65px.svg'
                   alt='Logo Kornhausforum'
-                  className='h-16'
+                  className='h-14 mr-4'
                 />
-              </Link>
+              </a>
             </li>
           </ul>
         </li>
