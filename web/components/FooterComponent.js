30b4474eb08189c91128d8f18e787cb1b4e31738
Daniel Lerch <daniel@daniellerch.ch>
2021-02-24 09:57:02 +0100

    new mobile footer

--- web/components/FooterComponent.js
+++ web/components/FooterComponent.js
@@ -14,15 +14,15 @@ export default function FooterComponent() {
             <img
               src='/logos/logo_hkb_footer_100px.svg'
               alt='Logo BFH'
-              className='h-24'
+              className='h-16 lg:h-24'
             />
           </Link>
         </li>
-        <li className='mb-2 lg:col-span-4 lg:flex lg:flex-col lg:justify-between'>
-          <p className='mb-4 mt-one lg:mt-0 text-small lg:text-small-dt  '>
+        <li className='mt-8 lg:mt-0 mb-2 lg:col-span-4 lg:flex lg:flex-col lg:justify-between'>
+          <p className='mb-1 lg:mb-4 mt-one lg:mt-0 text-small lg:text-small-dt  '>
             Partner
           </p>
-          <ul className='lg:flex lg:flex-row '>
+          <ul className='flex'>
             <li className='cursor-pointer mb-4 lg:mb-0 lg:mr-4'>
               <a
                 rel='noopener'
@@ -32,7 +32,7 @@ export default function FooterComponent() {
                 <img
                   src='/logos/1_HERZOG_footer_65px.svg'
                   alt='Link zu Herzog'
-                  className='h-14 mr-4'
+                  className='h-8 lg:h-14 mr-4'
                 />
               </a>
             </li>
@@ -45,7 +45,7 @@ export default function FooterComponent() {
                 <img
                   src='/logos/1_LoMarazzi_footer_65px.svg'
                   alt='Link zu Losinger Marazzi'
-                  className='h-14 mr-4'
+                  className='h-8 lg:h-14 mr-4'
                 />
               </a>
             </li>
@@ -54,7 +54,7 @@ export default function FooterComponent() {
                 <img
                   src='/logos/1_Hutwil_footer_65px.png'
                   alt='Link zu Huttwil'
-                  className='h-14 mr-4'
+                  className='h-8 lg:h-14 mr-4'
                 />
               </a>
             </li>
@@ -67,7 +67,7 @@ export default function FooterComponent() {
                 <img
                   src='/logos/1_SIA_footer_65px.png'
                   alt='Link zu SIA'
-                  className='h-14 mr-4'
+                  className='h-8 lg:h-14 mr-4'
                 />
               </a>
             </li>
@@ -80,18 +80,18 @@ export default function FooterComponent() {
                 <img
                   src='/logos/1_Kornhausforum_footer_65px.svg'
                   alt='Lnik zu Kornhausforum'
-                  className='h-14 mr-4'
+                  className='h-8 lg:h-14 mr-4'
                 />
               </a>
             </li>
           </ul>
         </li>
-        <li className='mb-2 lg:col-span-1'>
+        <li className='mb-1 lg:mb-2 lg:col-span-1'>
           <Link href='/impressum'>
             <a className='text-small lg:text-small-dt'>Impressum</a>
           </Link>
         </li>
-        <li className='mb-2 lg:col-span-1'>
+        <li className='mb-1 lg:mb-2 lg:col-span-1'>
           <Link href='/kontakt'>
             <a className='text-small lg:text-small-dt'>Kontakt</a>
           </Link>
