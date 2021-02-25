70a517304a884fa1803c069cc6ef2dcc0e8d1a4e
Daniel Lerch <daniel@daniellerch.ch>
2021-02-25 14:25:21 +0100

    zwistand animiert text bei einblenden

--- web/components/Lauftext.js
+++ web/components/Lauftext.js
@@ -1,5 +1,8 @@
 import BlockContent from '@sanity/block-content-to-react'
 import client from 'client'
+import { motion } from 'framer-motion'
+import useIntersectionObserver from 'lib/useIntersectionObserver'
+import { useRef, useState } from 'react'
 
 export default function Lauftext({ lauftext }) {
   const laufweite = lauftext.laufweite ? 'kompakt' : 'normal'
@@ -18,20 +21,41 @@ export default function Lauftext({ lauftext }) {
     },
   }
 
+  const lauftextSection = useRef(null)
+  const isVisible = useIntersectionObserver(lauftextSection)
+
   return (
     <div
+      ref={lauftextSection}
       key={lauftext._key}
       className={`lauftext mb-1 lg:mb-2 lg:grid lg:grid-cols-4 2xl:grid-cols-8`}
     >
-      <article
-        className={`${laufweite} lg:col-start-2 2xl:col-start-3 2xl:col-span-4  `}
-      >
-        <BlockContent
-          blocks={lauftext.inhalt}
-          serializers={serializers}
-          {...client.config()}
-        />
-      </article>
+      {isVisible && (
+        <motion.article
+          initial='hidden'
+          animate='visible'
+          variants={{
+            hidden: {
+              y: 10,
+              opacity: 0,
+            },
+            visible: {
+              y: 0,
+              opacity: 1,
+              transition: {
+                delay: 0.4,
+              },
+            },
+          }}
+          className={`${laufweite} lg:col-start-2 2xl:col-start-3 2xl:col-span-4  `}
+        >
+          <BlockContent
+            blocks={lauftext.inhalt}
+            serializers={serializers}
+            {...client.config()}
+          />
+        </motion.article>
+      )}
     </div>
   )
 }
