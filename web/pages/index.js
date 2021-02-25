9f76f986da34c8ab694b8108c569c5200b1c74a5
Daniel Lerch <daniel@daniellerch.ch>
2021-02-25 02:14:05 +0100

    index corrs done

--- web/pages/index.js
+++ web/pages/index.js
@@ -48,90 +48,92 @@ const Home = (props) => {
           <header className='fixed left-0 right-0 z-30 2xl:top-0 2xl:left-0 2xl:w-full'>
             <MainNavigation />
           </header>
-          <main className='fixed lg:left-4 2xl:left-6 lg:right-4 2xl:right-6 mt-20 z-20 text-white'>
-            <h1 className='base mb-2 text-white text-big lg:text-big-dt'>
+          <main className='fixed top-0 bottom-0 left-1 right-1 lg:left-4 lg:right-4 2xl:left-6 2xl:right-6 pt-20 pb-16 flex flex-col z-20 text-white'>
+            <h1 className='base mb-2 text-white text-big lg:text-big-dt z-20'>
               Neue Räume
               <br />
               Stadt und Land in Huttwil
             </h1>
-            <h2 className='mb-two'>Jahresausstellung Architektur 2021</h2>
-            {showNextButtons && (
-              <ul className='mt-20 lg:w-1/2' style={{ 'margin-left': '25%' }}>
-                <li className='text-left h-16 lg:h-20 cursor-pointer border-white border-2 p-2 mb-2 mr-1'>
-                  <Link href='/themenpfade'>
-                    <div className='flex'>
-                      <MdDirectionsWalk size='2em' />
-                      <p className='ml-4 text-left text-big lg:text-medium-dt 2xl:text-big-dt'>
-                        Folgen Sie einem Themenpfad
-                      </p>
-                    </div>
-                  </Link>
-                </li>
-                <li className='text-left h-16 lg:h-20 cursor-pointer border-white border-2 p-2  mb-2 mr-1'>
-                  <Link href='/map'>
-                    <div className='flex'>
-                      <MdMap size='2em' />
-                      <p className='ml-4 text-left text-big lg:text-medium-dt 2xl:text-big-dt'>
-                        Navigieren Sie auf der Karte
-                      </p>
-                    </div>
-                  </Link>
-                </li>
-                <li className='text-left h-16 lg:h-20 cursor-pointer border-white border-2 p-2  mb-2 mr-1'>
-                  <button onClick={() => setShowNextButtons(false)}>
-                    <div className='flex'>
-                      <MdReplay size='2em' />
-                      <p className='ml-4 text-left text-big lg:text-medium-dt 2xl:text-big-dt'>
-                        Videobotschat noch einmal ansehen
-                      </p>
-                    </div>
-                  </button>
-                </li>
-              </ul>
+            <h2 className='z-20'>Jahresausstellung Architektur 2021</h2>
+            <ul
+              className='lg:w-1/2 flex flex-col flex-1 justify-center'
+              id='nextButtons'
+            >
+              {showNextButtons && (
+                <>
+                  <li className='text-left flex items-center h-18 lg:h-20 cursor-pointer border-white border-2 p-2 mb-2'>
+                    <Link href='/themenpfade'>
+                      <div className='flex items-center'>
+                        <MdDirectionsWalk size='2em' />
+                        <p className='ml-4 text-left lg:text-medium-dt 2xl:text-big-dt'>
+                          Folgen Sie einem Themenpfad
+                        </p>
+                      </div>
+                    </Link>
+                  </li>
+                  <li className='text-left flex items-center h-18 lg:h-20 cursor-pointer border-white border-2 p-2  mb-2'>
+                    <Link href='/map'>
+                      <div className='flex items-center'>
+                        <MdMap size='2em' />
+                        <p className='ml-4 text-left lg:text-medium-dt 2xl:text-big-dt'>
+                          Navigieren Sie auf der Karte
+                        </p>
+                      </div>
+                    </Link>
+                  </li>
+                  <li className='text-left flex items-center h-18 lg:h-20 cursor-pointer border-white border-2 p-2  mb-2'>
+                    <button onClick={() => setShowNextButtons(false)}>
+                      <div className='flex items-center'>
+                        <MdReplay size='2em' />
+                        <p className='ml-4 text-left lg:text-medium-dt 2xl:text-big-dt'>
+                          Videobotschat noch einmal ansehen
+                        </p>
+                      </div>
+                    </button>
+                  </li>
+                </>
+              )}
+            </ul>
+            <a
+              rel='noopener'
+              href='https://www.bfh.ch/ahb/de/aktuell/veranstaltungen/vernissage-jaa-2021/'
+              target='_blank'
+              id='vernissage'
+              className='border-t-2 border-b-2 border-white text-white py-2 flex z-20'
+            >
+              <MdArrowForward size='2rem' className='' />
+              <article className='ml-4'>
+                <h2 className='mb-2'>Vernissage</h2>
+                <blockquote className='text-medium lg:text-big-dt'>
+                  02.03.2021, 18:00-18:30 Uhr – Online
+                </blockquote>
+              </article>
+            </a>
+            {videodoc && !showNextButtons && (
+              <div className='fullscreenVideo'>
+                <ReactPlayer
+                  url={`https://stream.mux.com/${videodoc.playbackId}.m3u8`}
+                  playing
+                  width='100%'
+                  height='100%'
+                  controls={true}
+                  onEnded={() => setShowNextButtons(true)}
+                  playIcon={
+                    <button aria-label='play video' className='fixed'>
+                      <MdPlayArrow color='white' size='10rem' />
+                    </button>
+                  }
+                  light='../videoStills/Startseite_TEST.gif'
+                />
+              </div>
             )}
           </main>
-          {videodoc && !showNextButtons && (
-            <div className='fullscreenVideo'>
-              <ReactPlayer
-                url={`https://stream.mux.com/${videodoc.playbackId}.m3u8`}
-                playing
-                width='100%'
-                height='100%'
-                controls={true}
-                onEnded={() => setShowNextButtons(true)}
-                playIcon={
-                  <button aria-label='play video'>
-                    <MdPlayArrow color='white' size='10rem' />
-                  </button>
-                }
-                light='../videoStills/Startseite_TEST.gif'
-              />
-            </div>
-          )}
-          <a
-            rel='noopener'
-            href='https://www.bfh.ch/ahb/de/aktuell/veranstaltungen/vernissage-jaa-2021/'
-            target='_blank'
-            id='vernissage'
-            className='fixed left-4 2xl:left-6 right-4 2xl:right-6 border-t-2 border-b-2 border-white text-white py-2 bottom-20 flex'
-          >
-            <MdArrowForward size='2rem' className='' />
-            <article className='ml-4'>
-              <h2 className='mb-2'>Vernissage</h2>
-              <blockquote className='text-medium lg:text-medium-dt'>
-                02.03.2021, 18:00-18:30 Uhr – Online
-              </blockquote>
-            </article>
-          </a>
         </div>
       </div>
       <style jsx global>{`
         body {
           background-color: black;
         }
-        // #vernissage {
-        //   backdrop-filter: blur(5px);
-        // }
         .fullscreenVideo video,
         .fullscreenVideo .react-player__preview {
           object-fit: cover;
@@ -140,6 +142,11 @@ const Home = (props) => {
           position: fixed;
           top: 0;
           left: 0;
+        }
+        @media screen and (min-width: 1024px) {
+          #nextButtons {
+            margin-left: 25%;
+          }
         }
       `}</style>
     </>
