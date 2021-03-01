580907026db1b35b0b7fe6bbbec0f3271b9206b6
Daniel Lerch <daniel@daniellerch.ch>
2021-03-01 12:59:24 +0100

    mux Video Abstand zu Figcap in Sfari test

--- web/components/MuxVideo.js
+++ web/components/MuxVideo.js
@@ -5,14 +5,14 @@ export default function MuxVideo({ video }) {
 
   return (
     <div className='lg:grid lg:grid-cols-4 mb-4'>
-      <div className='lg:col-start-2 lg:col-span-3 2xl:col-start-2 2xl:col-span-2 mb-4'>
+      <div className='lg:col-start-2 lg:col-span-3 2xl:col-start-2 2xl:col-span-2 mb-4 block'>
         <ReactPlayer
           url={`https://stream.mux.com/${id}.m3u8`}
           width='100%'
           height='100%'
           controls={true}
         />
-        <p className='text-small lg:text-small-dt mt-1 lg:mt-2'>
+        <p className='text-small lg:text-small-dt mt-1 lg:mt-2 block'>
           {video.title}
         </p>
       </div>
